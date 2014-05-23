70398f6ab894c5e0cdf4f6a306fa596fa640363a
axegang <fibonacci@zju.edu.cn>
2014-05-23 14:28:54 +0800

    add polymorphism.cc

---
+++ polymorphism.cc
@@ -0,0 +1,32 @@
+//No virtual function!
+#include<cstdio>
+
+class A
+{
+  public:
+    int x;
+    void print()
+    {
+      printf("In parent, x= %d\n", x);
+    }
+    A(): x(0){}
+};
+
+class B: public A
+{
+  public:
+    int x;
+    void print()
+    {
+      printf("In child, x= %d\n", x);
+    }
+    B():x(1){}
+};
+
+int main()
+{
+  A* p = new B;
+  p->print();
+  delete p;
+  return 0;
+}
